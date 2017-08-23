3ae86349f2786baa9af89f4bb2c7b553d7a02a41
jackhowa <johncarverhoward@gmail.com>
2017-08-23 16:37:22 -0500

    Sum for moving up in range between the two arrays

--- sum-range.js
+++ sum-range.js
@@ -1,5 +1,14 @@
 function sumAll(arr) {
-  return 1;
+  var sumOutput = 0;
+  var currentNum = 0;
+  console.log(currentNum);
+  while (currentNum < arr[1]) {
+    currentNum++;
+    console.log(currentNum);
+    sumOutput += currentNum;
+    console.log(sumOutput);
+  }
+  return sumOutput;
 }
 
 sumAll([1, 4]);
