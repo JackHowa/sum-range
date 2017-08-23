dd1fa5414885ee0fc8572864107fc57e1dd1bb4b
jackhowa <johncarverhoward@gmail.com>
2017-08-23 16:46:48 -0500

    Delete if else loop, Use programmatic thinking to find min/max

--- sum-range.js
+++ sum-range.js
@@ -1,11 +1,21 @@
 function sumAll(arr) {
   var sumOutput = 0;
-  var currentNum = arr[0];
-  while (currentNum <= arr[1]) {
-    sumOutput += currentNum;
-    currentNum++;
+  if (arr[0] < arr[1]) {
+    var currentNum = arr[0];
+    while (currentNum <= arr[1]) {
+      sumOutput += currentNum;
+      currentNum++;
+    }
+    return sumOutput;
   }
-  return sumOutput;
+  else {
+    var currentNum = arr[1];
+    while (currentNum >= arr[0]) {
+      sumOutput += currentNum;
+      currentNum--;
+    }
+    return sumOutput;
+  }
 }
 
 sumAll([1, 4]);
