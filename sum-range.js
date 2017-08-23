6ea161dc47b5010134a86485f6b5390a0a58a300
jackhowa <johncarverhoward@gmail.com>
2017-08-23 16:47:13 -0500

    Use min max to compare size in array

--- sum-range.js
+++ sum-range.js
@@ -1,21 +1,11 @@
 function sumAll(arr) {
   var sumOutput = 0;
-  if (arr[0] < arr[1]) {
-    var currentNum = arr[0];
-    while (currentNum <= arr[1]) {
-      sumOutput += currentNum;
-      currentNum++;
-    }
-    return sumOutput;
+  var currentNum = Math.min(arr[0],arr[1]);
+  while (currentNum <= Math.max(arr[0],arr[1])) {
+    sumOutput += currentNum;
+    currentNum++;
   }
-  else {
-    var currentNum = arr[1];
-    while (currentNum >= arr[0]) {
-      sumOutput += currentNum;
-      currentNum--;
-    }
-    return sumOutput;
-  }
+  return sumOutput;
 }
 
 sumAll([1, 4]);
